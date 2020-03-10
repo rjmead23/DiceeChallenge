6f81f78ca3159828afcae127f8403f6097c833cd
Richard Meadows <rich_meadows76@yahoo.com>
2020-03-09 22:50:25 -0700

    Add functions

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1 ;
-var randomDiceImage = "dice" + randomNumber1 + ".png";
-var randomImageSource = "images/" + randomDiceImage;
 
-var image1 = document.querySelectorAll("img")[0];
-image1.setAttribute("src", randomImageSource);
+// Player 1 Dice
+var randomNumber1 = selectRandomNumber(6) ;
+var randomImageSource = selectDiceImage(randomNumber1);
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
 
 
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+// Player 2 dice
+var randomNumber2 = selectRandomNumber(6);
+var randomImageSource2 = selectDiceImage(randomNumber2);
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 
+// Function Section
+function selectRandomNumber(numbers){
+  var randomNumber =  Math.floor(Math.random() * numbers) + 1;
+  return randomNumber
+}
 
+function selectDiceImage(number){
+  return "images/dice" + number + ".png";
+}
+
+// Update text depending on outcome
 if(randomNumber1 > randomNumber2){
   document.querySelector("h1").innerHTML = "Player 1 wins";
 }
