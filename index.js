32b67b184652acb98b175e202bdda7bd81700bb1
Richard Meadows <rich_meadows76@yahoo.com>
2020-03-09 22:57:37 -0700

    Add resultText function

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ var randomNumber1 = selectRandomNumber(6) ;
 var randomImageSource = selectDiceImage(randomNumber1);
 document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
 
-
 // Player 2 dice
 var randomNumber2 = selectRandomNumber(6);
 var randomImageSource2 = selectDiceImage(randomNumber2);
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
+displayResultText(randomNumber1, randomNumber2);
 
 // Function Section
 function selectRandomNumber(numbers){
@@ -21,13 +21,15 @@ function selectDiceImage(number){
   return "images/dice" + number + ".png";
 }
 
-// Update text depending on outcome
-if(randomNumber1 > randomNumber2){
-  document.querySelector("h1").innerHTML = "Player 1 wins";
-}
-else if(randomNumber2 > randomNumber1){
-  document.querySelector("h1").innerHTML = "Player 2 wins";
-}
-else{
-  document.querySelector("h1").innerHTML = "draw!";
+function displayResultText(number1, number2){
+  // Update text depending on outcome
+  if(number1 > number2){
+    document.querySelector("h1").innerHTML = "Player 1 wins";
+  }
+  else if(number2 > number1){
+    document.querySelector("h1").innerHTML = "Player 2 wins";
+  }
+  else{
+    document.querySelector("h1").innerHTML = "draw!";
+  }
 }
